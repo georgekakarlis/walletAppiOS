e875e9985f5bbbc17e96f37428716157b31d04c0
GeorgeK <georgekakarlis@yahoo.com>
2021-10-03 22:15:32 +0300

    Initial Commit

---
+++ walletApp/walletAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  walletAppApp.swift
+//  walletApp
+//
+//  Created by George Kakarlis on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct walletAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
